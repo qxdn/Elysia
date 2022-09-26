f1b81e227b46cd0ed0ae420da99ad2f3fc535bb6
Xu Qian <1464238196@qq.com>
2022-09-27 03:30:26 +0800

    🐞 fix(plugin): update plugin

--- Elysia/plugins/repeat/__init__.py
+++ Elysia/plugins/repeat/__init__.py
@@ -7,13 +7,14 @@ from .data_source import Repeat
 
 repeat = Repeat()
 
-repeat_matcher = repeat.on_message("复读机","自动复读消息",priority=10,block=False)
+repeat_matcher = repeat.on_message("复读机","自动复读消息",priority=10)
 
 
-@repeat_matcher.handle([Cooldown(3)])
+@repeat_matcher.handle([Cooldown(2)])
 async def repeat_message(bot:Bot,event:GroupMessageEvent):
     gid = event.group_id
     answer = repeat.repeatMessage(event)
     if answer != None:
         logger.info(f"[复读机]发送回复:{answer}")
         await bot.send_group_msg(group_id=gid,message=answer,auto_escape=False)
+    repeat_matcher.finish()
